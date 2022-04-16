0fb05d0ded86f9a7785a6a0b2ef1b12fe8238472
shreerajbhamare <shreerajbhamare01@gmail.com>
2022-04-16 21:27:47 +0530

    code,fix: Threads prototype, minor fixes

--- one-one/my_thread.h
+++ one-one/my_thread.h
@@ -1,1 +1,10 @@
 #include "thrd_struct.h"
+
+//Defination for Thread create
+int thread_create(mythread *, void *(*target_funtion)(void *), void *);
+//Defination of Thread Join
+int thread_join(mythread *, void **);
+//Defination for Thread Exit
+void thread_exit(void *);
+//Defination for Thread Kill
+int thread_kill(mythread, int);

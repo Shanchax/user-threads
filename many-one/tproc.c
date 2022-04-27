e4e861c59098b6331119833a7b73f427c0c54f2c
shantanuc <chakrabortyss19.comp@coep.ac.in>
2022-04-27 10:50:10 +0530

    fix:minor fixes

--- many-one/tproc.c
+++ many-one/tproc.c
@@ -1,27 +1,35 @@
-#include"tproc.h"
-#include<stdlib.h>
+
+#include "tproc.h"
+#include <stdlib.h>
 #include <sys/mman.h>
 
 #define STACKSIZE (64*1024)
 
-TPROC* create_tcb(void){
-	static int next_id;
-	TPROC* new;
+//function to create a "thread control block", named as tproc by us(named after struct proc in xv6.)
+//@params:void
+//@returns:pointer to the newly created "tproc"
+TPROC *create_tcb(void){
+    static int next_id = 1;
+    TPROC *new;
 
-	if((new = calloc(1,sizeof(TPROC))) == NULL){
-		return NULL;
-	}
-	
-	new->id = next_id++;
-	return new;
+    if ((new = calloc(1, sizeof(TPROC))) == NULL) {
+	    return NULL;
+    }
+    
+    new->id = next_id++;
+    return new;
 }
 
-void delete_tcb(TPROC*block){
-	if(block->has_dynamic_stack==1){
-		//free(block->context.uc_stack.ss_sp);
-		munmap(block->thread_context.uc_stack.ss_sp,STACKSIZE);
-	}
+//function to delete given tproc
+//@params: pointer to the tproc to be deleted
+//@returns:(idealy we intended to use mprotect to return errno on failure. 
+//however it's causing segfault)
+void delete_tcb(TPROC *block)
+{
+    if (block->has_dynamic_stack) {
+    munmap(block->context.uc_stack.ss_sp,STACKSIZE);
+    }
 
-	free(block);
+    free(block);
 }
 
